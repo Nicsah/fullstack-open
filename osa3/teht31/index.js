e9861584beca5566eb1fbbe9067c08d0ecf28cb5
Nicsah <nico.j.saari@gmail.com>
2024-07-30 01:48:57 +0300

    Fullstack osa 3 render tehtävä

--- osa3/teht31/index.js
+++ osa3/teht31/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const app = express()
+app.use(express.static('dist'))
 var morgan = require('morgan')
 const cors = require('cors')
-const app = express()
 app.use(cors())
 app.use(express.json())
 
@@ -85,7 +86,7 @@ app.post('/api/persons',(request,response) => {
 })
         
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
