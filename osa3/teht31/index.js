50e23263da22db659f6a2118db8aa5b3206ba715
Nicsah <nico.j.saari@gmail.com>
2024-07-30 22:27:02 +0300

    uibuild

--- osa3/teht31/index.js
+++ osa3/teht31/index.js
@@ -4,47 +4,36 @@ var morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
-const person = require('./models/person')
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 
-
-
-let numbers =[
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    },
-]
-
-
 morgan.token('body', (req) => JSON.stringify(req.body))
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const errorHandler = (error,request,response,next) => {
+    console.error(error.message)
+
+    if (error.name ==='CastError') {
+        return response.status(400).send({error:'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
+}
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 
 app.get('/api/persons',(request,response, next) => {
     Person.find({}).then(persons => {
-    response.json(persons)
+        response.json(persons)
     })
-    .catch(error => next(error))
+        .catch(error => next(error))
 })
 
 app.get('/info',(request,response,next) => {
@@ -60,73 +49,68 @@ app.get('/info',(request,response,next) => {
 
 app.get('/api/persons/:id',(request,response,next) => {
     Person.findById(request.params.id)
-    .then(person => {
-        if (person) {
-        response.json(person)
-        }else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+        .then(person => {
+            if (person) {
+                response.json(person)
+            }else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id',(request,response, next) => {
     Person.findByIdAndDelete(request.params.id)
-    .then(person => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons',(request,response) => {
+app.post('/api/persons',(request,response, next) => {
     const body = request.body
 
     if (!body.name || !body.number ) {
         return response.status(400).json ({
             error: 'content missing'
-       })
+        })
     }
 
     const person = new Person({
-        "name": body.name,
-        "number": body.number
+        'name': body.name,
+        'number': body.number
     })
     
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 
 })
 
 app.put('/api/persons/:id',(request, response, next) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-        response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+    const {name, number} = request.body
+
+    Person.findByIdAndUpdate(
+        request.params.id,
+        {name, number},
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => {
+            if (updatedPerson) {
+                response.json(updatedPerson)
+            }else {
+                response.status(404).json({
+                    error: 'person not found'
+                })
+            }
+        })
+        .catch(error => next(error))
 })
-
-const errorHandler = (error,request,response,next) => {
-    console.error(error.message)
-
-    if (error.name ==='CastError') {
-        return response.status(400).send({error:'malformatted id'})
-    }
-    next(error)
-}
-app.use(errorHandler)
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
   
- 
 app.use(unknownEndpoint)
+app.use(errorHandler)
         
 
 const PORT = process.env.PORT
--- osa3/teht31/models/person.js
+++ osa3/teht31/models/person.js
@@ -6,16 +6,40 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to',url)
 mongoose.connect(url)
-.then(() => {
-    console.log('connected to MongoDB')
-})
-.catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-})
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const numeroValidator = (luku) => {
+    const parts = luku.split('-')
+    if (parts.length !== 2) {
+        return false
+    }
+    if (parts[0].length>3 || parts[0].length<2){
+        return false
+    }
+
+    return true
+}
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name:{
+        type:String,
+        required:true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate : {
+            validator: numeroValidator,
+            message: 'number is not valid, here is example of right way: 040-1234567'       
+        }
+    },
 })
 
 personSchema.set('toJSON', {
